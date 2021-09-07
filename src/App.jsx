06f707626355e6c388ee430c2ccfb2ea770a5d56
zhanggl94 <zhanggl94@163.com>
2021-09-07 22:46:48 +0800

    update: 项目初始化一部分文件

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header" style={{ 'background-color': 'red' }}>header</header>
+      <div className="left-sidebar" style={{ 'background-color': 'green' }}>leftsidebar</div>
+      <main className="main" style={{'background-color':'gray'}}>main</main>
+      <footer className="footer" style={{'background-color':'yellow'}}>footer</footer>
+    </div>
+  );
+}
+
+export default App;
