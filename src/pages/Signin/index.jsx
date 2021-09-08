5fcdb841afffedee17960322e74b7d74a0495147
zhanggl <zhanggl94@163.com>
2021-09-08 13:50:58 +0800

    add: 添加antd并按需引入

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import './index.less'
 export default class index extends Component {
   render() {
     return (
-      <div>
-        login
+      <div className="change">
       </div>
     )
   }
