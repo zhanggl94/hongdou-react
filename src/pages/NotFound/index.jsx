b9b175e53b61cfdceb16066b9ef4dbf952e40cb6
zhanggl <zhanggl94@163.com>
2021-09-24 11:15:55 +0800

    修改404页面

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Button } from 'antd'
+import { useHistory } from 'react-router-dom'
+import './index.less'
 
-export default function index() {
+export default function NotFound() {
+    const history = useHistory();
+    // 返回首页
+    const goHome = () => {
+        history.push('/hongdou')
+    }
+    
     return (
         <div>
-            <h2>404</h2>
+            <div className="not-found">
+                <div>
+                    <span className="not-found-word-4">4</span>
+                    <span className="not-found-word-0">0</span>
+                    <span className="not-found-word-4">4</span></div>
+                <div>
+                    <Button onClick={goHome}>回到首页</Button>
+                </div>
+            </div>
         </div>
     )
 }
